9170153fd1182ca90e038dd23bad9686eef376ce
Ozie Virto <ovirto@outlook.com>
2024-10-20 02:11:47 -0500

    Row of 3 X's

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,12 @@ import { useState} from "react";
 
 
 export default function Square() {
-    return <button className="square">X</button>
+    return (
+        <>
+            <button className="square">X</button>
+            <button className="square">X</button>
+            <button className="square">X</button>
+        </>
+    );
+
 }
