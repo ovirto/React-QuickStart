5186c5c68465c62e8f158221a7415fccbc0d0647
Ozie Virto <ovirto@outlook.com>
2024-10-19 20:23:59 -0500

    Updating the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,33 @@
 import React from 'react';
 import './App.css';
-
-
-
-// Displaying Data
-const user = {
-    name: 'Hedy Lamarr',
-    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-    imageSize: 90,
-};
+import { useState} from "react";
 
 
 
 // Creating and nesting components
 function MyButton() {
+    // Updating the screen
+    const [count, setCount] = useState(0);
+
     // Responding to events
     function handleClick() {
-        alert('You cliked me!');
+        setCount(count + 1);
     }
     return (
         <button onClick={handleClick}>
-            Click me
+            Clicked {count} times
         </button>
     );
 }
 
 
-
-// Writing markup with JSX
-function AboutPage() {
-    return (
-        <>
-            <h1>About</h1>
-            <p>Hello there.<br />How do you do?</p>
-        </>
-    );
-}
-
 function App() {
   return (
       <div className="App">
           <img className="avatar"/>
-          <h1>Welcome to My App</h1>
+          <h1>Counters that update separately</h1>
           <MyButton/>
-          <AboutPage/>
+          <MyButton/>
       </div>
   );
 }
@@ -51,55 +35,3 @@ function App() {
 
 
 export default App;
-
-
-
-/*
-// Displaying Data
-export default function Profile() {
-    return (
-        <>
-            <h1>{user.name}</h1>
-            <img
-                className="avatar"
-                src={user.imageUrl}
-                alt={'Photo of ' + user.name}
-                style={{
-                    width: user.imageSize,
-                    height: user.imageSize
-                }}
-            />
-        </>
-    );
-}
-
- */
-
-
-
-/*
-// Rendering Lists
-const products = [
-    { title: 'Cabbage', isFruit: false, id: 1 },
-    { title: 'Garlic', isFruit: false, id: 2 },
-    { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export default function ShoppingList() {
-    const listItems = products.map(product =>
-        <li
-            key={product.id}
-            style={{
-                color: product.isFruit ? 'magenta' : 'darkgreen'
-            }}
-        >
-            {product.title}
-        </li>
-    );
-
-    return (
-        <ul>{listItems}</ul>
-    );
-}
-
- */
