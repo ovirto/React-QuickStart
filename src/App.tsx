995617f898fe84fa5897a867cca856abf43b32af
Ozie Virto <ovirto@outlook.com>
2024-10-21 01:00:00 -0500

    Declaring a winner

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ import { useState } from 'react';
 function Square({value, onSquareClick}) {
     return (
         <button className="square" onClick={onSquareClick}>
-        {value}
-    </button>
+            {value}
+        </button>
     );
 }
-
-
 
 export default function Board() {
     const [xIsNext, setXIsNext] = useState(true);
@@ -19,23 +17,30 @@ export default function Board() {
 
     // @ts-ignore
     function handleClick(i) {
-        if (squares[i]) {
+        if (calculateWinner(squares) || squares[i]) {
             return;
         }
-
         const nextSquares = squares.slice();
         if (xIsNext) {
-            nextSquares[i] = "X";
+            nextSquares[i] = 'X';
         } else {
-            nextSquares[i] = "O";
+            nextSquares[i] = 'O';
         }
-
         setSquares(nextSquares);
         setXIsNext(!xIsNext);
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -53,4 +58,25 @@ export default function Board() {
             </div>
         </>
     );
+}
+
+// @ts-ignore
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
