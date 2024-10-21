46d24eba9a00abd5ac093f1c314294af52993cba
Ozie Virto <ovirto@outlook.com>
2024-10-21 02:42:57 -0500

    Final cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -63,9 +63,9 @@ function Board({ xIsNext, squares, onPlay}) {
 
 
 export default function Game() {
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[currentMove];
 
     // @ts-ignore
@@ -73,13 +73,11 @@ export default function Game() {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
-        setXIsNext(!xIsNext);
     }
 
     // @ts-ignore
     function jumpTo(nextMove){
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0);
     }
 
     const moves = history.map((squares, move) => {
