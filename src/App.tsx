386592cd48c537aabda28bfe61b3a005f0dd613d
Ozie Virto <ovirto@outlook.com>
2024-10-21 00:24:17 -0500

    Making an interactive component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
 import React from 'react';
 import './App.css';
-import { useState} from "react";
+import { useState } from 'react';
 
+function Square() {
+    const [value, setValue] = useState(null);
 
-// @ts-ignore
-function Square({ value }) {
-    return <button className="square">{value}</button>;
+    function handleClick() {
+        // @ts-ignore
+        setValue('X');
+    }
+
+    return (
+        <button
+            className="square"
+            onClick={handleClick}
+        >
+            {value}
+        </button>
+    );
 }
 
 export default function Board() {
     return (
         <>
             <div className="board-row">
-                <Square value="1" />
-                <Square value="2" />
-                <Square value="3" />
+                <Square />
+                <Square />
+                <Square />
             </div>
             <div className="board-row">
-                <Square value="4" />
-                <Square value="5" />
-                <Square value="6" />
+                <Square />
+                <Square />
+                <Square />
             </div>
             <div className="board-row">
-                <Square value="7" />
-                <Square value="8" />
-                <Square value="9" />
+                <Square />
+                <Square />
+                <Square />
             </div>
         </>
     );
-
 }
