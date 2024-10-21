f7cda7918f8b1cbdaf0b5f4a926b477f4ef3af52
Ozie Virto <ovirto@outlook.com>
2024-10-21 02:24:33 -0500

    Lifting state up, again

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,10 @@ function Square({value, onSquareClick}) {
     );
 }
 
-export default function Board() {
-    const [xIsNext, setXIsNext] = useState(true);
-    const [squares, setSquares] = useState(Array(9).fill(null));
+
+
+// @ts-ignore
+function Board({ xIsNext, squares, onPlay}) {
 
     // @ts-ignore
     function handleClick(i) {
@@ -26,8 +27,7 @@ export default function Board() {
         } else {
             nextSquares[i] = 'O';
         }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext);
+        onPlay(nextSquares);
     }
 
     const winner = calculateWinner(squares);
@@ -59,6 +59,33 @@ export default function Board() {
         </>
     );
 }
+
+
+
+export default function Game() {
+    const [xIsNext, setXIsNext] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const currentSquares = history[history.length - 1];
+
+    // @ts-ignore
+    function handlePlay(nextSquares) {
+        setHistory([...history, nextSquares]);
+        setXIsNext(!xIsNext);
+    }
+
+    return (
+        <div className="game">
+            <div className ="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+            </div>
+            <div className ="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
+    );
+}
+
+
 
 // @ts-ignore
 function calculateWinner(squares) {
